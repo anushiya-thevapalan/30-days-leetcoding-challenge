37edef5a1a52704c11d42ae110b6ce543c2d0612
anushiya-thevapalan <anushiya.16@cse.mrt.ac.lk>
2020-04-12 16:45:20 +0530

    solving day 12 problem of leetcoding challenge

---
+++ 12.Last_Stone_Weight.py
@@ -0,0 +1,35 @@
+'''
+Day 12 - 30 days Leetcoding Challenge - April 12, 2020
+
+Problem (leetcode-1046)
+
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+ 
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+'''
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while stones:
+            if len(stones) == 1:
+                return stones[0]
+            stones.sort(reverse = True)
+            if stones[0] - stones[1] != 0:
+                stones.append(stones[0] - stones[1])
+            stones = stones[2:]
+        return 0
